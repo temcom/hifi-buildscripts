7f5d03a65b729196d580b1290488b35b53ee914e
Joe Large [Omega Heron] <OmegaHeron@users.noreply.github.com>
2017-10-27 19:30:51 -0400

    Create artful_buildhf.sh

---
+++ artful/artful_buildhf.sh
@@ -0,0 +1,14 @@
+#
+# Build script for High Fidelity VR Platform - Ubuntu Artful edition.
+# (c) 2017 J.C.L -- aka the virtual persona, Omega Heron.
+# GPL 3.0
+#
+# No support will be given, use at your own risk and read all instructions before using.
+#
+# If you would like to improve this script then by all means offer a pull request.
+# Do not file comments saying you should do this or that, do it yourself and offer a pull request.
+# There's no assurance this will work ongoing -- It depends upon High Fidelity not breaking its
+# make files (a routine occurence) and certain packages being at certain level in Artful.  It's likely
+# Artful will remain compatible through its lifespan (Summer 2018) - unless HiFi moves to a new
+# bleeding edge.
+#
