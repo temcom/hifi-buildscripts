cd3cdf4a564fcbea1ae1d52eb805a7c2a03a393e
Joe Large [Omega Heron] <OmegaHeron@users.noreply.github.com>
2017-10-27 19:28:36 -0400

    Create tumbleweed_hfbuild.sh

---
+++ Tumbleweed/tumbleweed_hfbuild.sh
@@ -0,0 +1,14 @@
+#
+# Build script for High Fidelity VR Platform - openSUSE Tumbleweed edition.
+# (c) 2017 J.C.L -- aka the virtual persona, Omega Heron.
+# GPL 3.0
+#
+# No support will be given, use at your own risk and read all instructions before using.
+#
+# If you would like to improve this script then by all means offer a pull request.
+# Do not file comments saying you should do this or that, do it yourself and offer a pull request.
+# There's no assurance this will work ongoing -- It depends upon High Fidelity not breaking its
+# make files (a routine occurence) and certain packages being at certain level in Tumbleweed (not
+# an assured proposition since Tumbleweed is all about the bleeding edge -- it just so happens that
+# its current bleeding edge aligns perfectly with what HiFi needs).
+#
