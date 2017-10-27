7041269291f9495ea89b8318d03476cdf54a4791
Joe Large [Omega Heron] <OmegaHeron@users.noreply.github.com>
2017-10-27 19:42:41 -0400

    Create get-latest-tag.sh

---
+++ common/get-latest-tag.sh
@@ -0,0 +1,16 @@
+#
+# Build script for High Fidelity VR Platform - Utility function.
+# (c) 2017 J.C.L -- aka the virtual persona, Omega Heron.
+# GPL 3.0
+#
+# No support will be given, use at your own risk and read all instructions before using.
+#
+# If you would like to improve this script then by all means offer a pull request.
+# Do not file comments saying you should do this or that, do it yourself and offer a pull request.
+# There's no assurance this will work ongoing -- It depends upon High Fidelity not changing how it
+# tags its release build points.
+#
+#!/bin/sh
+export HFSRC=~/src.hifi
+cd $HFSRC
+/usr/bin/git describe --tags $(git rev-list --tags --max-count=1) | sed 's/RELEASE-//g'
