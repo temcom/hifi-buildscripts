c040402869bc3e305be76d1766bc849b01487bb6
Joe Large [Omega Heron] <OmegaHeron@users.noreply.github.com>
2017-10-29 17:54:57 -0400

    Update tumbleweed_hfbuild.sh

--- Tumbleweed/tumbleweed_hfbuild.sh
+++ Tumbleweed/tumbleweed_hfbuild.sh
@@ -16,8 +16,8 @@
 # This looks for most recent tag and builds or warns and exits if already built.
 set -e
 export QT_CMAKE_PREFIX_PATH=/usr/lib64/cmake
-export HFSRC=$USER/src/hifi
-export HFBINBASE=$USER/hifi
+export HFSRC=/home/$USER/src/hifi
+export HFBINBASE=/home/$USER/hifi
 export CXX='g++ -std=c++11'
 if [ ! -e $HFSRC ]; then
         mkdir -p $HFSRC
