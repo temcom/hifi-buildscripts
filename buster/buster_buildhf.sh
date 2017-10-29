0d5400a742a0b044c8a817bff32ac5bec8812ab5
Joe Large [Omega Heron] <OmegaHeron@users.noreply.github.com>
2017-10-29 13:05:09 -0400

    Create buster_buildhf.sh

---
+++ buster/buster_buildhf.sh
@@ -0,0 +1,12 @@
+
+# Build script for High Fidelity VR Platform - Debian 10 (Buster / Testing) edition.
+# (c) 2017 J.C.L -- aka the virtual persona, Omega Heron.
+# GPL 3.0
+#
+# No support will be given, use at your own risk and read all instructions before using.
+#
+# If you would like to improve this script then by all means offer a pull request.
+# Do not file comments saying you should do this or that, do it yourself and offer a pull request.
+# There's no assurance this will work ongoing -- It depends upon High Fidelity not breaking its
+# make files (a routine occurence) and certain packages being at certain level in Buster.
+#
