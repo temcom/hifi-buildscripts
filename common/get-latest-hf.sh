1b4a0ffd541bd2b17ad6e9d97b93aca4c1a4aede
Joe Large [Omega Heron] <OmegaHeron@users.noreply.github.com>
2017-10-29 18:49:47 -0400

    Update get-latest-hf.sh

--- common/get-latest-hf.sh
+++ common/get-latest-hf.sh
@@ -10,9 +10,14 @@
 # There's no assurance this will work ongoing -- It depends upon High Fidelity not changing how it
 # tags its release build points.
 #
+# This is a simple way to find what most recent "tagged" release HF is at.  It could be used as a
+# basis for something like checking via a cronjob and then building if a new source tag is out there.
+#
+# I'll sort out how to check for RC (formal release builds) vs daily (DEV) builds... eventually.
+#
 #!/bin/sh
 # You must set HFSRC to location of your High Fidelity source.
-export HFSRC=~/src/hifi
+HFSRC=/home/$USER/src/hifi
 cd $HFSRC
 export RELEASE_NUMBER=$(/usr/bin/git describe --tags $(git rev-list --tags --max-count=1) | sed 's/RELEASE-//g')
 echo $RELEASE_NUMBER
